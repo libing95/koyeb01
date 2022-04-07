0e4260ffd6f72fbbcecda045836e252468394304
libing95 <56621603+libing95@users.noreply.github.com>
2022-04-07 13:49:52 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "5d9852fc-5793-4127-a32d-47cd50b71723",
                         "alterId": 0
                     }
                 ],
